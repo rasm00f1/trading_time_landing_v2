74942f680a96c0d28ec1668251d655170c15d453
rasmus petersen <rasm00f1@stud.kea.dk>
2022-02-22 22:35:00 +0100

    small fix

--- src/Landing/Gallerypopup.js
+++ src/Landing/Gallerypopup.js
@@ -1,6 +1,6 @@
 export default function Gallerypopup(props) {
   return (
-    <section className="popup_background">
+    <div className="popup_background">
       <div className="scale_in popup">
         <div className="popup_img">
           <div className="side_by_side">
@@ -16,6 +16,6 @@ export default function Gallerypopup(props) {
           <img src={`./img/${props.currentImg.galleryimg}.jpg`} alt="popup version" />
         </div>
       </div>
-    </section>
+    </div>
   );
 }
